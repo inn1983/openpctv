959fb66562c2fd5484cf0ba459bb4a72bcc2a3f0
Henry Liu <relaxbsd@gmail.com>
2013-11-05 10:17:24 +0800

    update to devel-20131104

--- packages/lirc/scripts/lircd_pre.sh
+++ packages/lirc/scripts/lircd_pre.sh
@@ -13,25 +13,34 @@ fi
 
 for i in 0 1 2 3 4 5 6 7 8 9; do
   [ -d /sys/class/rc ] && break
-  sleep 0.2
+  sleep 0.3
 done
 
 for i in 0 1 2 3 4 5 6 7 8 9; do
   ls /sys/class/rc/* >/dev/null 2>&1 && break
-  sleep 0.2
+  sleep 0.3
 done
 
 FIRSTIR="yes"
 for i in /sys/class/rc/*; do
-  [ $FIRSTIR == "yes" ] && DEVEVENT2=$(ls -d $i/input*)
-  grep -i "$DRV_NAME" $i/uevent >/dev/null 2>&1 && DEVEVENT1=$(ls -d $i/input*)
+  [ $FIRSTIR == "yes" ] && DEVEVENT2=$(ls -d $i/lirc*)
+  grep -i "$DRV_NAME" $i/uevent >/dev/null 2>&1 && DEVEVENT1=$(ls -d $i/lirc*)
    FIRSTIR="no"
 done
 
 if [ "X$DEVEVENT1" == "X" ]; then
-  DEVEVENT=${DEVEVENT2##*input}
+  DEVEVENT=$(basename ${DEVEVENT2})
 else
-  DEVEVENT=${DEVEVENT1##*input}
+  DEVEVENT=$(basename ${DEVEVENT1})
 fi
 
-sed -i "s/^DEVICE=.*/DEVICE=\"\/dev\/input\/event$DEVEVENT\"/g" /etc/lirc/hardware.conf
+if [ "X$DEVEVENT" == "X" ]; then
+  DEVEVENT=lirc0
+fi
+
+DEVNUM=${DEVEVENT##*lirc}
+if [ -f /sys/class/rc/rc${DEVNUM}/protocols ]; then
+  echo "lirc" > /sys/class/rc/rc${DEVNUM}/protocols
+fi
+
+sed -i "s/^DEVICE=.*/DEVICE=\"\/dev\/$DEVEVENT\"/g" /etc/lirc/hardware.conf
