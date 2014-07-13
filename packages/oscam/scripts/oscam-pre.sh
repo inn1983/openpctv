5c8ede1ec9e0bc66a2207253de734db7b8e36fa2
Henry Liu <relaxbsd@gmail.com>
2014-07-13 12:18:45 +0800

    update to 1.0RC5

--- packages/oscam/scripts/oscam-pre.sh
+++ packages/oscam/scripts/oscam-pre.sh
@@ -7,17 +7,13 @@
 target=$(systemctl get-default)
 if (grep -v -q "systemd.unit" /proc/cmdline && test X$target = "Xenigma2pc.target") || grep -q enigma2pc /proc/cmdline; then
   target=enigma2pc
-  for i in 15 14 13 12 11 10 9 8 7 6 5 4 3 2 1; do
+  for i in 10 9 8 7 6 5 4 3 2 1; do
     if [ -c /dev/dvb/adapter0/dvr0 -a -c /dev/dvb/adapter0/frontend0 ]; then
        [ $(lsmod | grep -c dvbsoftwareca) -eq 0 ] && modprobe dvbsoftwareca
        sleep 0.1
-       for j in /dev/dvb/adapter*; do
-         [ -L $j/dvr1 ] || ln -s $j/dvr0 $j/dvr1
-         [ -L $j/demux1 ] || ln -s $j/demux0 $j/demux1
-       done
        break
     else
-       sleep 0.6
+       sleep 0.5
     fi
   done
   [ -c /dev/dvb/adapter0/ca0 ] || (sleep 1;rmmod dvbsoftwareca; modprobe dvbsoftwareca)
